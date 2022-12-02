2e54d8215bffe70d76d0ea361a2ee02cb966c40f
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-02 21:31:26 +0200

    Update main.js

--- 01-box-arrow/js/main.js
+++ 01-box-arrow/js/main.js
@@ -5,18 +5,20 @@ let moveStepX = 0;
 let moveStepY = 0;
 function onPress(e) {
   const key = e.code;
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const marginLeft = parseInt(box.style.marginLeft);
+  const marginTop = parseInt(box.style.marginTop);
 
-  if (key === 'ArrowLeft') {
+  if (key === 'ArrowLeft' && marginLeft > 0) {
     moveStepX -= 10;
-    box.style.marginLeft = `${moveStepX}px`;
-  } else if (key === 'ArrowRight') {
+  } else if (key === 'ArrowRight' && windowWidth - 140 > marginLeft) {
     moveStepX += 10;
-    box.style.marginLeft = `${moveStepX}px`;
-  } else if (key === 'ArrowDown') {
+  } else if (key === 'ArrowDown' && windowHeight - 200 > marginTop) {
     moveStepY += 10;
-    box.style.marginTop = `${moveStepY}px`;
-  } else if (key === 'ArrowUp') {
+  } else if (key === 'ArrowUp' && marginTop > 0) {
     moveStepY -= 10;
-    box.style.marginTop = `${moveStepY}px`;
   }
+  box.style.marginLeft = `${moveStepX}px`;
+  box.style.marginTop = `${moveStepY}px`;
 }
